632230b4bd6cd4419fa8cd7b5acffceff9896f2d
Md Minhaz Uddin <md.minhazuddin2597@gmail.com>
2026-05-02 14:08:22 +0600

    Arrow function example added

--- src/function.ts
+++ src/function.ts
@@ -17,3 +17,33 @@ function greet(name: string, title?: string): string {
 }
 
 console.log(greet("Minhaz"));
+
+// Function with default paramenter
+function multiply(a: number, b: number = 2): number {
+  return a * b;
+}
+console.log(multiply(2));
+
+// Arrow function
+
+// Implicit return (single expression)
+const addArrow = (a: number, b: number): number => a + b;
+
+console.log(addArrow(2, 2));
+
+// Explicit return with block body
+const addArrowExplicit = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(addArrowExplicit(3, 7));
+
+// Single paramenter (parentheses optional)
+const square = (x: number): number => x * x;
+
+// With optional paramenter
+const greetArrow = (name: string, title?: string): string => {
+  return title ? `Hello, ${title} ${name}` : `Hello, ${name}`;
+};
+
+console.log(greetArrow("Sir", "Assalamualaikum"));
